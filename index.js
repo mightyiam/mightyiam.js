3ab5b8657d3d91d37604715b774c395807677dc7
Shahar Or <mightyiampresence@gmail.com>
2015-02-19 19:21:04 +0200

    More data

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-module.exports.authorString = 'Shahar Or <mightyiampresence@gmail.com> (http://mightyi.am)'
+var exports = module.exports
+
+exports.firstName = 'Shahar'
+exports.lastName = 'Or'
+exports.fullName = exports.firstName + ' ' + exports.lastName
+
+exports.website = 'http://mightyi.am'
+exports.email = 'mightyiampresence@gmail.com'
+
+exports.github = {}
+exports.github.username = 'mightyiam'
+exports.github.profileUrl = 'https://github.com/' + exports.github.username
+
+module.exports.authorString = exports.fullName + ' <' + exports.email + '> (' +
+    exports.email + ')'
